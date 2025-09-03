d9677ecc6e3d17595991b79566e0c825753d8438
Vinícius <vinicavequi@gmail.com>
2025-09-03 15:28:43 -0300

    commit inicial

---
+++ biblioteca/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+using biblioteca.Data;
+using MySql.Data.MySqlClient;
+
+namespace biblioteca.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly Database db = new Database();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CriarUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CriarUsuario(Usuario vm)
+        {
+            using var conn = db.GetConnection();
+            using var cmd = new MySqlCommand("sp_usuario_criar", conn);
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("p_nome", vm.nome);
+            cmd.Parameters.AddWithValue("p_email", vm.email);
+            cmd.Parameters.AddWithValue("p_senha_hash", vm.senha_hash);
+            cmd.Parameters.AddWithValue("p_role", vm.role);
+
+            cmd.ExecuteNonQuery();
+
+            return RedirectToAction("CriarUsuario");
+        }
+    }
+}
---
+++ biblioteca/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace biblioteca.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string? nome { get; set; }
+        public string? email { get; set; }
+        public string? senha_hash { get; set; }
+        public string? role { get; set; }
+        public Int16 ativo { get; set; }
+        public DateTime criado_em { get; set; }
+
+    }
+}
